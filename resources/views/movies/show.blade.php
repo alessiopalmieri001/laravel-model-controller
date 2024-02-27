26d38860c508fe368b8db55ab8f2ff9f25c90508
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2024-02-27 09:13:17 +0100

    fix + finito esercizio

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,20 +3,32 @@
 @section('page-title', $movie->title)
 
 @section('main-content')
-<h1>
+<h1 class="text-center">
     {{ $movie->title }}
 </h1>
 
 <div class="row">
     <div class="col-12">
         <div class="card">
-            <div class="card-body">
+            <div class="card-body text-center ">
                 <h3>
                     {{ $movie->title }}
                 </h3>
 
                 <p>
                     {{ $movie->author }}
+                </p>
+                <p>
+                    {{ $movie->original_title }}
+                </p>
+                <p>
+                    {{ $movie->nationality }}
+                </p>
+                <p>
+                    {{ $movie->date }}
+                </p>
+                <p>
+                    {{ $movie->vote}}
                 </p>
             </div>
         </div>
